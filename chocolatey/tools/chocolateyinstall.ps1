4cbbac275dcbb77b0968e3463029618bf556ef83
Ronit Rameja <ronitrameja28@gmail.com>
2025-08-04 11:35:14 +0530

    updated versions

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'lazytrack'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/master-wayne7/lazytrack/releases/download/v0.0.0/lazytrack_Windows_x86_64.zip'
-$checksum = '0000000000000000000000000000000000000000000000000000000000000000'
+$url = 'https://github.com/master-wayne7/lazytrack/releases/download/v1.0.0/lazytrack_Windows_x86_64.zip'
+$checksum = '37F8152842697A85E0B244695B4199564CD9D9A2CE10AD11D964B245209A4466'
 $checksumType = 'sha256'
 
 $packageArgs = @{
